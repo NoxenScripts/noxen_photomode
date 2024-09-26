76deaa1af4e5041889cf1b9c1503de473d86f1c9
Rubylium <19718604+Rubylium@users.noreply.github.com>
2024-09-26 09:52:56 +0200

    Refactor camera controls and add mouse movement functionality

--- client/client.lua
+++ client/client.lua
@@ -4,20 +4,16 @@ PHOTOMODE.CameraName = "photoModeCam"
 PHOTOMODE.FOV = 0
 
 function PHOTOMODE.Start()
-    -- Get gameplay camera properties
     local gameplayCamPos = GetFinalRenderedCamCoord()
-    local gameplayCamRot = GetGameplayCamRot(2) -- Rotation in degrees
+    local gameplayCamRot = GetGameplayCamRot(2)
     local gameplayCamFov = GetGameplayCamFov()
     PHOTOMODE.FOV = gameplayCamFov
 
     print(gameplayCamPos)
-    -- Create a new camera using the Cam library
     Cam.Create(PHOTOMODE.CameraName)
     Cam.SetPosition(PHOTOMODE.CameraName, gameplayCamPos)
     Cam.SetRotation(PHOTOMODE.CameraName, gameplayCamRot, 2)
     Cam.SetFov(PHOTOMODE.CameraName, gameplayCamFov)
-    
-    -- Set the new camera as active
     Cam.SetActive(PHOTOMODE.CameraName, true, false, 0)
     
     PHOTOMODE.IsActive = true
@@ -27,40 +23,31 @@ function PHOTOMODE.Start()
             local pPed = PlayerPedId()
             Cam.HandleSmartDof(PHOTOMODE.CameraName, pPed, 1.0)
     
-            -- Check for mouse scroll input to adjust FOV
-            if IsControlJustPressed(0, 241) then -- Scroll up
-                local scaleFactor = gameplayCamFov / 20.0 -- Adjust the divisor to control sensitivity
-                gameplayCamFov = math.max(gameplayCamFov - scaleFactor, 1.0) -- Decrease FOV, minimum 1.0
-                PlaySoundFrontend(-1, "NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET", true) -- Play sound
-            elseif IsControlJustPressed(0, 242) then -- Scroll down
-                local scaleFactor = gameplayCamFov / 20.0 -- Adjust the divisor to control sensitivity
-                gameplayCamFov = math.min(gameplayCamFov + scaleFactor, 120.0) -- Increase FOV, maximum 120.0
-                PlaySoundFrontend(-1, "NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET", true) -- Play sound
+            if IsControlJustPressed(0, 241) then
+                local scaleFactor = gameplayCamFov / 20.0
+                gameplayCamFov = math.max(gameplayCamFov - scaleFactor, 1.0)
+                PlaySoundFrontend(-1, "NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET", true)
+            elseif IsControlJustPressed(0, 242) then
+                local scaleFactor = gameplayCamFov / 20.0
+                gameplayCamFov = math.min(gameplayCamFov + scaleFactor, 120.0)
+                PlaySoundFrontend(-1, "NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET", true)
             end
     
             PHOTOMODE.FOV = Utils.CalculateNextScalablePosition(gameplayCamFov, PHOTOMODE.FOV, 0.1)
             Cam.SetFov(PHOTOMODE.CameraName, PHOTOMODE.FOV)
     
-            -- Get mouse movement to move the camera
-            local mouseX = GetControlNormal(0, 1) -- INPUT_LOOK_LR
-            local mouseY = GetControlNormal(0, 2) -- INPUT_LOOK_UD
+            local mouseX = GetControlNormal(0, 1)
+            local mouseY = GetControlNormal(0, 2)
     
-            -- Retrieve the current FOV
             local currentFov = GetCamFov(Cam.Cache[PHOTOMODE.CameraName])
     
-            -- Define a scaling factor based on the current FOV
-            local scaleFactor = currentFov / 40.0 -- Adjust the divisor to control sensitivity
+            local scaleFactor = currentFov / 40.0
     
-
-            -- Check if the left mouse button is held down
-            if IsDisabledControlPressed(0, 24) then -- Left mouse button
-                -- Adjust camera position based on mouse movement
-                print("pressed")
+            if IsDisabledControlPressed(0, 24) then
                 local camPos = GetCamCoord(Cam.Cache[PHOTOMODE.CameraName])
                 camPos = vector3(camPos.x + mouseX * 0.1, camPos.y, camPos.z - mouseY * 0.1)
                 Cam.SetPosition(PHOTOMODE.CameraName, camPos)
             else
-                -- Adjust camera rotation based on mouse movement and scaling factor
                 local camRot = GetCamRot(Cam.Cache[PHOTOMODE.CameraName], 2)
                 camRot = vector3(camRot.x - mouseY * 5.0 * scaleFactor, camRot.y, camRot.z - mouseX * 5.0 * scaleFactor)
                 Cam.SetRotation(PHOTOMODE.CameraName, camRot, 2)
@@ -71,7 +58,6 @@ function PHOTOMODE.Start()
         PHOTOMODE.IsActive = false
     end)
 end
-
 function PHOTOMODE.BlockMouvementsControls()
     if PHOTOMODE.IsActive then
         DisableControlAction(0, 30, true) -- INPUT_MOVE_LR
