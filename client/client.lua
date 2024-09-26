de0d8b0069ccc4495dad9c7584604d1100952d70
Rubylium <19718604+Rubylium@users.noreply.github.com>
2024-09-26 10:48:56 +0200

    Refactor camera controls and add mouse movement functionality

--- client/client.lua
+++ client/client.lua
@@ -43,15 +43,35 @@ function PHOTOMODE.Start()
     
             local scaleFactor = currentFov / 40.0
     
-            if IsDisabledControlPressed(0, 24) then
-                local camPos = GetCamCoord(Cam.Cache[PHOTOMODE.CameraName])
-                local camRot = GetCamRot(Cam.Cache[PHOTOMODE.CameraName], 2)
-                local forwardVector = RotationToDirection(camRot)
-                local rightVector = RotationToDirection(vector3(camRot.x, camRot.y, camRot.z + 90.0))
-                camPos = camPos + forwardVector * (-mouseY * 0.1) + rightVector * (mouseX * 0.1)
-                Cam.SetPosition(PHOTOMODE.CameraName, camPos)
-            else
-                local camRot = GetCamRot(Cam.Cache[PHOTOMODE.CameraName], 2)
+            local camPos = GetCamCoord(Cam.Cache[PHOTOMODE.CameraName])
+            local camRot = GetCamRot(Cam.Cache[PHOTOMODE.CameraName], 2)
+            local forwardVector = RotationToDirection(camRot)
+            local upVector = vector3(0.0, 0.0, 1.0)
+            -- Right vector calculation using cross-product to ensure correct horizontal movement
+            local rightVector = CrossProduct(forwardVector, upVector)
+    
+            if IsDisabledControlPressed(0, 32) then -- W key
+                camPos = camPos + forwardVector * 0.1
+            end
+            if IsDisabledControlPressed(0, 33) then -- S key
+                camPos = camPos - forwardVector * 0.1
+            end
+            if IsDisabledControlPressed(0, 34) then -- A key
+                camPos = camPos - rightVector * 0.1
+            end
+            if IsDisabledControlPressed(0, 35) then -- D key
+                camPos = camPos + rightVector * 0.1
+            end
+            if IsDisabledControlPressed(0, 44) then -- Q key
+                camPos = camPos - upVector * 0.1
+            end
+            if IsDisabledControlPressed(0, 45) then -- E key
+                camPos = camPos + upVector * 0.1
+            end
+    
+            Cam.SetPosition(PHOTOMODE.CameraName, camPos)
+    
+            if not IsDisabledControlPressed(0, 24) then
                 camRot = vector3(camRot.x - mouseY * 5.0 * scaleFactor, camRot.y, camRot.z - mouseX * 5.0 * scaleFactor)
                 Cam.SetRotation(PHOTOMODE.CameraName, camRot, 2)
             end
@@ -61,6 +81,15 @@ function PHOTOMODE.Start()
         SetTimeScale(1.0)
         PHOTOMODE.IsActive = false
     end)
+end
+
+-- Function to calculate the cross product of two vectors
+function CrossProduct(v1, v2)
+    return vector3(
+        v1.y * v2.z - v1.z * v2.y,
+        v1.z * v2.x - v1.x * v2.z,
+        v1.x * v2.y - v1.y * v2.x
+    )
 end
 
 function RotationToDirection(rotation)
