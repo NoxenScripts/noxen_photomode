a414f17b1ff427b3d8fdd36060141766241bdc13
Rubylium <19718604+Rubylium@users.noreply.github.com>
2024-09-26 17:28:46 +0200

    Refactor client.lua: Add hooks for entering and exiting photomode

--- client/client.lua
+++ client/client.lua
@@ -38,6 +38,7 @@ function PHOTOMODE.Start()
     DisplayHud(false)
     DisplayRadar(false)
     Config.EnteredPhotomode()
+    TriggerServerEvent("photomode:SetPlayerInPhotomode")
 
     PHOTOMODE.IsActive = true
     Citizen.CreateThread(function()
@@ -183,6 +184,7 @@ function PHOTOMODE.Start()
         end
         SetTimeScale(1.0)
         PHOTOMODE.IsActive = false
+        TriggerServerEvent("photomode:RemovePlayerInPhotomode")
 
         -- Show HUD and radar
         DisplayHud(true)
@@ -229,7 +231,6 @@ function PHOTOMODE.Stop()
     Cam.Destroy(PHOTOMODE.CameraName)
 end
 
-
 -- Commands
 RegisterCommand("photomode", function()
     if not PHOTOMODE.IsActive then
@@ -238,3 +239,59 @@ RegisterCommand("photomode", function()
         PHOTOMODE.Stop()
     end
 end, false)
+
+local PlayersPedInPhotomode = {}
+-- Player filtering thread
+Citizen.CreateThread(function()
+    while true do
+        for k,v in pairs(GetActivePlayers()) do
+            local sID = GetPlayerServerId(v)
+            if PHOTOMODE.PlayersInPhotomode[sID] then
+                local pPed = GetPlayerPed(v)
+                if not PlayersPedInPhotomode[sID] then
+                    PlayersPedInPhotomode[sID] = pPed
+                end
+            else
+                if PlayersPedInPhotomode[sID] then
+                    PlayersPedInPhotomode[sID] = nil
+                end
+            end
+        end
+
+        Wait(3000)
+    end
+end)
+
+-- Main thread for icon display
+Citizen.CreateThread(function()
+    local x, y = UI.ConvertToPixel(30, 30)
+    while true do
+        local HandlingUI = false
+        local pCoords = GetEntityCoords(PlayerPedId())
+
+        for k,v in pairs(PlayersPedInPhotomode) do
+            local entityCoords = GetEntityCoords(v)
+            local distance = #(pCoords - entityCoords)
+            if distance <= 15 then
+                HandlingUI = true
+
+                UI.DrawSimpleSprite("photomode_ui", "camera_icon", 0.5, 0.5, x, y, 0, 255, 255, 255, 255, {Draw3d = {pos = vector3(entityCoords.x, entityCoords.y, entityCoords.z + 1.0)}})
+                print("Drawing icon", entityCoords, x, y)
+            end
+        end
+
+        if HandlingUI then
+            Wait(1)
+        else
+            Wait(500)
+        end
+    end
+end)
+
+RegisterNetEvent("photomode:SetPlayerInPhotomode", function(serverID)
+    PHOTOMODE.PlayersInPhotomode[serverID] = true
+end)
+
+RegisterNetEvent("photomode:RemovePlayerInPhotomode", function(serverID)
+    PHOTOMODE.PlayersInPhotomode[serverID] = nil
+end)
