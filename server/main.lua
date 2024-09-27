5d970aa3516c2c9747204d1d474fde39f98b85ce
Rubylium <19718604+Rubylium@users.noreply.github.com>
2024-09-27 15:12:04 +0200

    reformat

--- server/main.lua
+++ server/main.lua
@@ -8,47 +8,49 @@ end)
 
 -- Server-side command logging with integrated permissions check
 RegisterCommand("photomode", function(source, args, rawCommand)
-    if source > 0 then  -- Checks if the command is executed by a player (source > 0)
+    if source == 0 then
+        return
+    end -- Checks if the command is executed by a player (source > 0)
 
-        local hasPermission = false
+    local hasPermission = false
 
-        -- Job check (if enabled in config)
-        if Config.CheckJob then
-            local jobName = API.GetPlayerJob(source)
-            for _, allowedJob in ipairs(Config.AllowedJobs) do
-                if jobName == allowedJob then
-                    hasPermission = true
-                    break
-                end
-            end
-        end
-
-        -- Group check (if enabled in config)
-        if Config.CheckGroup then
-            local group = API.GetPlayerGroup(source)
-            for _, allowedGroup in ipairs(Config.AllowedGroups) do
-                if group == allowedGroup then
-                    hasPermission = true
-                    break
-                end
-            end
-        end
-
-        -- VIP status check (if enabled in config)
-        if Config.CheckVIP then
-            local isVIP = API.IsPlayerVIP(source)
-            if isVIP then
+    -- Job check (if enabled in config)
+    if Config.CheckJob then
+        local jobName = API.GetPlayerJob(source)
+        for _, allowedJob in ipairs(Config.AllowedJobs) do
+            if jobName == allowedJob then
                 hasPermission = true
+                break
             end
-        end
-
-        -- If the player has the necessary permissions
-        if hasPermission then
-            -- Send event to customer to activate or deactivate photo mode
-            TriggerClientEvent('photomode:toggleMode', source)
-        else
-            -- Notification to player that he does not have the necessary permissions
-            Config.SendNotification(source, Config.NoPermissionMessage)
         end
     end
-end, false)  -- false means that the command is not restricted via ACE by default
+
+    -- Group check (if enabled in config)
+    if Config.CheckGroup then
+        local group = API.GetPlayerGroup(source)
+        for _, allowedGroup in ipairs(Config.AllowedGroups) do
+            if group == allowedGroup then
+                hasPermission = true
+                break
+            end
+        end
+    end
+
+    -- VIP status check (if enabled in config)
+    if Config.CheckVIP then
+        local isVIP = API.IsPlayerVIP(source)
+        if isVIP then
+            hasPermission = true
+        end
+    end
+
+    -- If the player has the necessary permissions
+    if hasPermission then
+        -- Send event to customer to activate or deactivate photo mode
+        TriggerClientEvent('photomode:toggleMode', source)
+    else
+        -- Notification to player that he does not have the necessary permissions
+        Config.SendNotification(source, Config.NoPermissionMessage)
+    end
+
+end, false) -- false means that the command is not restricted via ACE by default
